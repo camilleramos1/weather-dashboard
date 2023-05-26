ef1c37ba9d01b7476c276b4b143f1b25769e4452
camilleramos1 <129894673+camilleramos1@users.noreply.github.com>
2023-05-26 09:56:09 -0600

    added HTML and CSS styling

---
+++ assets/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    // personal API key for OpenWeather 
+    const apiKey = 'b756e5aa291f3d2dcd5b230b03a8d666';
+    // variables for left container
+    const searchHistory = $('.search-history')
+    const cityInput = $('#city-input')
+    // var to store searched cities
+    let pastCities = [];
+    // variables for right container
+    const currentCity = $('#city');
+    const dateEl = $('#date');
+    const weatherEl = $('#weather');
+    const temperatureEL = $('#temperature');
+    const humidityEl = $('#humidity');
+    const windEl = $('#wind-speed');
+
+    const currentDate = moment().format('L');
+    
+
+
+
+
+
+
+
+});
+
+
+
+
+
